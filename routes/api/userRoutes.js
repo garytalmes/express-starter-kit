94a4405c44bd1f4098225870fb3c2ba98082aad7
Gary T Almes <garytalmes@gmail.com>
2023-05-05 13:45:40 -0400

    Added login route.

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -4,8 +4,8 @@ const User = require("../../models/User");
 
 router.get("/", (req, res) => {
   User.findAll()
-    .then( resp => res.json({ status: "success", payload: resp }))
-    .catch( err => res.json({ msg: err.message }))
+    .then( resp => res.status(200).json({ status: "success", payload: resp }))
+    .catch( err => res.status(200).json({ msg: err.message }))
 })
 
 
@@ -18,8 +18,8 @@ router.get("/:id", (req, res) => {
 
 router.post("/", (req, res) => {
   User.create(req.body)
-    .then( resp => res.json({ status: "success", payload: resp }))
-    .catch( err => res.json({ msg: err.message }))
+    .then( resp => res.status(200).json({ status: "success", payload: resp }))
+    .catch( err => res.status(200).json({ msg: err.message }))
 })
 
 router.put("/:id", (req, res) => {
@@ -31,8 +31,8 @@ router.put("/:id", (req, res) => {
       }
     }
   )
-    .then( resp => res.json({ status: "success", payload: resp }))
-    .catch( err => res.json({ msg: err.message }))
+    .then( resp => res.status(200).json({ status: "success", payload: resp }))
+    .catch( err => res.status(200).json({ msg: err.message }))
 })
 
 router.delete("/:id", (req, res) => {
@@ -41,8 +41,21 @@ router.delete("/:id", (req, res) => {
       id: req.params.id
     }
   })
-  .then( resp => res.json({ status: "success", payload: resp }))
-  .catch( err => res.json({ msg: err.message }))
+  .then( resp => res.status(200).json({ status: "success", payload: resp }))
+  .catch( err => res.status(200).json({ msg: err.message }))
+})
+
+
+// Here's a route you can use for authenticating a login
+router.post("/login", async (req, res) => {
+  const foundUser = await User.findOne({
+    where: {
+      email: req.body.email
+    }
+  })
+  if( !foundUser ) return res.status(401).json({ status: "error", msg: "No user found" })
+  if( !foundUser.validatePassword(req.body.password) ) return res.status(401).json({ status: "error", msg: "No user found" })
+  return res.status(200).json({ status: "success", payload: foundUser })
 })
 
 module.exports = router
